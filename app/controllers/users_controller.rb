590302ca8661b45de3f9a25fcaf356f64553edb6
callunity <heather.e.armstrong@gmail.com>
2014-09-30 14:10:03 -0400

    Created User model and controller, User_Sessions controller and associated routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+
+    if @user.save
+      redirect_to root_path
+    else
+      render :new, alert: "Please try again"
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :name)
+  end
+end
