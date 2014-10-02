ec221be5887cb0380ed2df005b231429d513bd86
emmack <eckotzer@gmail.com>
2014-10-02 18:24:48 -0400

    more js

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -17,79 +17,48 @@ $(document).on('ready page:load', function () {
 L.mapbox.accessToken = 'pk.eyJ1IjoiZWNrb3R6ZXIiLCJhIjoiMndTTGtIdyJ9.14KHP67dI919VFxtfr9eIQ';
    var map = L.mapbox.map('map', 'eckotzer.4a2f194e')
     .setView([43.6525, -79.381667], 10);
-var layers = document.getElementById('menu-ui');
+    var layers = document.getElementById('menu-ui');
+
 var popup = new L.Popup({ autoPan: false });
 
 
-var baselayer = L.geoJson(nodata,{
+addLayer(L.geoJson(nodata,{
       style: getStyle,
-      onEachFeature: onEachFeature
-  }).addTo(map);
+      // onEachFeature: onEachFeature
+  }), 'Base Map', 1);
 
-   function getStyle(feature) {
+addLayer(L.geoJson(vismin,{
+      // style: getStyle,
+      // onEachFeature: onEachFeature
+  }), 'Visible Minority', 2);
+
+addLayer(L.geoJson(lowinc,{
+      style: getStyle,
+      // onEachFeature: onEachFeature
+  }), 'Low Income', 3);
+
+ 
+
+  function getStyle(feature) {
+    if(layer.name ==="nodata" ){
+       return {
+          weight: 2,
+          opacity: 0.1,
+          color: 'black',
+          fillOpacity: 1,
+          fillColor: white
+      };
+    }
+      else if(layer.name ==="lowinc"){
       return {
           weight: 2,
           opacity: 0.1,
           color: 'black',
           fillOpacity: 1,
-          fillColor: 'white'
+          fillColor: getColor(feature.properties.lowinc)
       };
-  }
-
- function onEachFeature(feature, layer) {
-      layer.on({
-          mousemove: mousemove,
-          mouseout: mouseout,
-          click: zoomToFeature
-      });
-  }
-
- 
-
- var closeTooltip;
-
-  function mousemove(e) {
-      var layer = e.target;
-
-
-      popup.setLatLng(e.latlng);
-
-      if (!popup._map) popup.openOn(map);
-      window.clearTimeout(closeTooltip);
-
-      // highlight feature
-      layer.setStyle({
-          weight: 1,
-          opacity: 0.5,
-          fillOpacity: 0.9
-      });
-
-      if (!L.Browser.ie && !L.Browser.opera) {
-          layer.bringToFront();
-      }
-  }
-
-  function mouseout(e) {
-      baselayer.resetStyle(e.target);
-      closeTooltip = window.setTimeout(function() {
-          map.closePopup();
-      }, 100);
-  }
-
-  function zoomToFeature(e) {
-      map.fitBounds(e.target.getBounds());
-  }
-
-
-
-
-var visminlayer = L.geoJson(vismin, {
-      style: getStyle,
-      onEachFeature: onEachFeature
-  }).addTo(map);
-
-
-  function getStyle(feature) {
+    }
+     else if(layer.name ==="vismin"){
       return {
           weight: 2,
           opacity: 0.1,
@@ -97,52 +66,76 @@ var visminlayer = L.geoJson(vismin, {
           fillOpacity: 1,
           fillColor: getColor(feature.properties.vismin)
       };
+    }
   }
 
-    function getColor(d) {
-      return d > 100  ? '#cc4c02' :
-          d > 80 ? '#ec7014' :
-          d > 60  ? '#fe9929' :
-          d > 50   ? '#fec44f' :
+  function getColor(name, d) {
+   if(name === 'Low Income'){
+          return d > 60  ? '#cc4c02' :
+          d > 50 ? '#ec7014' :
+          d > 40  ? '#fe9929' :
+          d > 30  ? '#fec44f' :
+          d > 20   ? '#fee391' :
+          d > 10  ? '#fff7bc' :
+          '#ffffe5';
+    }else if(name === 'Visible Minority'){
+          return d > 100 ? '#ec7014' :
+          d > 80  ? '#fe9929' :
+          d > 60   ? '#fec44f' :
           d > 40   ? '#fee391' :
           d > 20   ? '#fff7bc' :
           '#ffffe5';
+    }else{
+      return false
+    }
   }
+  
 
 
- function onEachFeature(feature, layer) {
-      layer.on({
-          mousemove: mousemove,
-          mouseout: mouseout,
-          click: zoomToFeature
-      });
-  }
+ // function onEachFeature(feature, layer) {
+ //      layer.on({
+ //          // mousemove: mousemove,
+ //          mouseout: mouseout,
+ //          click: zoomToFeature
+ //      });
+ //  }
 
  var closeTooltip;
 
-  function mousemove(e) {
-      var layer = e.target;
+  // function mousemove(e) {
+  //     var layer = e.target;
 
-      popup.setLatLng(e.latlng);
-      popup.setContent(layer.feature.properties.low_inc_CO + 'Percent visible minority');
+  //     if (name === 'Low Income') {
+  //      var hover = layer.feature.properties.low_inc_CO;
+  //      console.log(hover)
+  //     }
+  //     else if (name === 'Visible Minority'){
+  //      var hover = layer.feature.properties.lots copy_;
+  //     };
+  //     else{
+  //       return false
+  //     }
 
-      if (!popup._map) popup.openOn(map);
-      window.clearTimeout(closeTooltip);
+  //     popup.setLatLng(e.latlng);
+  //     popup.setContent(hover + name);
 
-      // highlight feature
-      layer.setStyle({
-          weight: 1,
-          opacity: 0.3,
-          fillOpacity: 0.9
-      });
+  //     if (!popup._map) popup.openOn(map);
+  //     window.clearTimeout(closeTooltip);
 
-      if (!L.Browser.ie && !L.Browser.opera) {
-          layer.bringToFront();
-      }
-  }
+  //     // highlight feature
+  //     layer.setStyle({
+  //         weight: 1,
+  //         opacity: 0.3,
+  //         fillOpacity: 0.9
+  //     });
 
-  function mouseout(e) {
-      visminlayer.resetStyle(e.target);
+  //     if (!L.Browser.ie && !L.Browser.opera) {
+  //         layer.bringToFront();
+  //     }
+  // }
+
+  function mouseout(layer, e) {
+     layer.resetStyle(e.target);
       closeTooltip = window.setTimeout(function() {
           map.closePopup();
       }, 100);
@@ -155,7 +148,25 @@ var visminlayer = L.geoJson(vismin, {
   map.legendControl.addLegend(getLegendHTML());
 
   function getLegendHTML() {
-    var grades = [0, 20, 40, 50, 60, 80, 100],
+     if(name === 'Low Income'){
+      var grades = [0, 10, 20, 30, 40, 50, 60],
+    labels = [],
+    from, to;
+
+    for (var i = 0; i < grades.length; i++) {
+      from = grades[i];
+      to = grades[i + 1];
+
+      labels.push(
+        '<li><span class="swatch" style="background:' + getColor(from + 1) + '"></span> ' +
+        from + (to ? '&ndash;' + to : '+')) + '</li>';
+    }
+
+    return '<span>Percent low income. </span><ul>' + labels.join('') + '</ul>';
+  }
+
+else if (name === 'Visible Minority'){
+      var grades = [0, 20, 40, 60, 80, 100],
     labels = [],
     from, to;
 
@@ -170,130 +181,43 @@ var visminlayer = L.geoJson(vismin, {
 
     return '<span>Percent visible minority.</span><ul>' + labels.join('') + '</ul>';
   }
-
-
-
-//lowinc map
-var lowinclayer = L.geoJson(lowincome, {
-      style: getStyle,
-      onEachFeature: onEachFeature
-  }).addTo(map);
-
-
-
-  function getStyle(feature) {
-      return {
-          weight: 2,
-          opacity: 0.1,
-          color: 'black',
-          fillOpacity: 1,
-          fillColor: getColor(feature.properties.low_inc_CO)
-      };
+  else{
+    return false
   }
-
-    function getColor(d) {
-      return d > 60  ? '#cc4c02' :
-          d > 50 ? '#ec7014' :
-          d > 40  ? '#fe9929' :
-          d > 30   ? '#fec44f' :
-          d > 20   ? '#fee391' :
-          d > 10   ? '#fff7bc' :
-          '#ffffe5';
-  }
-
-
- function onEachFeature(feature, layer) {
-      layer.on({
-          mousemove: mousemove,
-          mouseout: mouseout,
-          click: zoomToFeature
-      });
-  }
-
- var closeTooltip;
-
-  function mousemove(e) {
-      var layer = e.target;
-
-      popup.setLatLng(e.latlng);
-      popup.setContent(layer.feature.properties.low_inc_CO + 'Prevalence of low income in 2010 based on after-tax low-income measure');
-
-      if (!popup._map) popup.openOn(map);
-      window.clearTimeout(closeTooltip);
-
-      // highlight feature
-      layer.setStyle({
-          weight: 1,
-          opacity: 0.3,
-          fillOpacity: 0.9
-      });
-
-      if (!L.Browser.ie && !L.Browser.opera) {
-          layer.bringToFront();
-      }
-  }
-
-  function mouseout(e) {
-      lowinclayer.resetStyle(e.target);
-      closeTooltip = window.setTimeout(function() {
-          map.closePopup();
-      }, 100);
-  }
-
-  function zoomToFeature(e) {
-      map.fitBounds(e.target.getBounds());
-  }
-
-  map.legendControl.addLegend(getLegendHTML());
-
-  function getLegendHTML() {
-    var grades = [0, 10, 20, 30, 40, 50, 60],
-    labels = [],
-    from, to;
-
-    for (var i = 0; i < grades.length; i++) {
-      from = grades[i];
-      to = grades[i + 1];
-
-      labels.push(
-        '<li><span class="swatch" style="background:' + getColor(from + 1) + '"></span> ' +
-        from + (to ? '&ndash;' + to : '+')) + '</li>';
-    }
-
-    return '<span>Prevalence of low income in 2010 based on after-tax low-income measure</span><ul>' + labels.join('') + '</ul>';
   }
 
 
 
-// L.control.layers(baselayer, visminlayer, lowinclayer).addTo(map);
+function addLayer(layer, name, zIndex) {
 
+    layer
+        .setZIndex(zIndex)
+        .addTo(map);
 
-  var link = document.getElementById('clickingEvent')
-    
+    var link = document.createElement('a');
+        link.href = '#';
+        link.className = 'active';
+        link.innerHTML = name;
 
-  $(link).on('click', function(e) {
-    e.preventDefault();
-    var type = link.innerHTML;
-    if (type = "Visible Minority"){
-      map.addLayer(vismin);
-    }
-      else if (type = "Prevalence of Low Income"){
-      map.addLayer(lowinclayer);
+    link.onclick = function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        if (map.hasLayer(layer)) {
+            map.removeLayer(layer);
+            this.className = '';
+        } else {
+            map.addLayer(layer);
+            this.className = 'active';
+        }
     };
 
-  })
-
+    layers.appendChild(link);
+}
 
 });
 
 
 
-//  if (map.hasLayer(layer)) {
-//             map.removeLayer(layer);
-//             this.className = '';
-//         } else {
-//             map.addLayer(layer);
-//             this.className = 'active';
-//         }
 
 
