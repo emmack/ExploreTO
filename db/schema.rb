b298b89033e73fbeebdf5a2da2edeef06d0ff073
callunity <heather.e.armstrong@gmail.com>
2014-10-02 17:50:46 -0400

    Added Name and Format fields to Data Set model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141002193356) do
+ActiveRecord::Schema.define(version: 20141002213622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20141002193356) do
     t.string   "file"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
+    t.string   "format"
   end
 
   create_table "favourites", force: true do |t|
