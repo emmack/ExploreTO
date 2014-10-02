a41526b89d1c089e608ab4c9ed261980086a6e9e
callunity <heather.e.armstrong@gmail.com>
2014-10-02 17:50:24 -0400

    Added Data Sets show

--- app/controllers/data_sets_controller.rb
+++ app/controllers/data_sets_controller.rb
@@ -1,13 +1,8 @@
 class DataSetsController < ApplicationController
-  before_filter :require_login
+
   
-  def download
-    send_file(specific_document) # sanitize params
-    @data_set.favourite_data.downloaded = true
+  def show
+    @data_set = DataSet.find(params[:id])
   end
 
-  private
-  def specific_document # sanitize params
-    @data_set.document_file
-  end
 end
