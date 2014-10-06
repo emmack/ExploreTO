976a8946f4b5327b9d18263013592aa63a8ae344
callunity <heather.e.armstrong@gmail.com>
2014-10-06 12:48:50 -0400

    Editing views and favourites controller to work with tags. Not fully functional yet.

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -23,10 +23,23 @@ class FavouritesController < ApplicationController
   end
 
   def show
-    @favourite = current_user.favourites
+    @favourite = Favourite.find(params[:id])
   end
 
-  def download # not quite working yet — want it to download file and also mark downloaded as true
+  def edit
+    @favourite = Favourite.find(params[:id])
+  end
+
+  def update
+    @favourite = Favourite.find(params[:id])
+    if @favourite.tag_list.add(params[:tag_list], parse: true)
+      redirect_to user_favourite_path[current_user.id, @favourite.id]
+    else 
+      render :edit, alert: "Something went wrong!"
+    end
+  end
+
+  def download 
     @favourite = Favourite.find(params[:favourite])
     @favourite.data_set = DataSet.find(params[:data_set])
     send_file "#{Rails.root}/public#{@favourite.data_set.file}"
@@ -34,9 +47,14 @@ class FavouritesController < ApplicationController
     @favourite.save
   end
 
+  def destroy
+    @favourite = Favourite.find(params[:id])
+    @favourite.destroy
+  end
+
   private
   def favourite_params
-    params.require(:favourite).permit(:data_set)
+    params.require(:favourite).permit(:data_set, :tag_list)
   end
 
 end
--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -1,4 +1,6 @@
 class Favourite < ActiveRecord::Base
+  acts_as_taggable
+  scope :by_join_date, order("created_at DESC")
 
   belongs_to :user
   belongs_to :data_set
