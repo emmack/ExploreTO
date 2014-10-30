6c493ae5dd28a983e308cc001e305a076aacb382
callunity <heather.e.armstrong@gmail.com>
2014-10-30 14:31:43 -0400

    Maps/Graphs drop down menu functional

--- app/assets/javascripts/controllers/users.js
+++ app/assets/javascripts/controllers/users.js
@@ -1,8 +1,19 @@
 $(document).on('ready page:load', function () {
-$('.js-accordion-trigger').bind('click', function(e){
-  jQuery(this).parent().find('.submenu').slideToggle('fast'); 
- // apply the toggle to the ul
-  jQuery(this).parent().toggleClass('is-expanded');
-  e.preventDefault();
-});
+  $('.submenu').hide();
+
+  $('.graphs-trigger').bind('click', function(e){
+    $('.submenu').hide();
+    $('.graphs-submenu').slideToggle('fast'); 
+   // apply the toggle to the ul
+    $(this).parent().toggleClass('is-expanded');
+    e.preventDefault();
+  });
+
+  $('.maps-trigger').bind('click', function(e){
+    $('.submenu').hide();
+    $('.maps-submenu').slideToggle('fast'); 
+   // apply the toggle to the ul
+    $(this).parent().toggleClass('is-expanded');
+    e.preventDefault();
+  });
 });
